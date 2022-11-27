955f3f57e60ca3c0943f78ba082c196005604b2a
nicvs <nichik.v@protonmail.com>
2022-11-27 16:32:23 +0300

    add days 18

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -2,6 +2,15 @@ package day18;
 
 public class Task1 {
     public static void main(String[] args) {
+        int[] numbers = {402, -50, -249, 215, 665, 311};
 
+        System.out.println(recursionSum(numbers,numbers.length));
+
+    }
+    public static int recursionSum(int[] numbers, int length){
+        if (length-1 < 0){
+            return 0;
+        }
+        return numbers[length-1]+recursionSum(numbers,length-1);
     }
 }
--- src/main/java/day18/Task3.java
+++ src/main/java/day18/Task3.java
@@ -2,6 +2,53 @@ package day18;
 
 public class Task3 {
     public static void main(String[] args) {
+        Node root = new Node(20);
+        addNew(14,root);
+        addNew(23,root);
+        addNew(11,root);
+        addNew(16,root);
+        addNew(22,root);
+        addNew(27,root);
+        addNew(5,root);
+        addNew(15,root);
+        addNew(18,root);
+        addNew(24,root);
+        addNew(150,root);
+        addNew(8,root);
 
+        dfs(root);
+    }
+
+    public static void addNew (int value, Node root){
+        Node newNode = new Node(value);
+        Node parent = root;
+        Node tempRoot;
+        while (true){
+            if (value < parent.getValue()){
+                tempRoot = parent.getLeftChild();
+                if (tempRoot == null){
+                    parent.setLeftChild(newNode);
+                    return;
+                }
+            } else {
+                tempRoot = parent.getRightChild();
+                if (tempRoot == null){
+                    parent.setRightChild(newNode);
+                    return;
+                }
+            }
+            parent = tempRoot;
+        }
+    }
+    public static void dfs(Node root){
+
+        if (root.getLeftChild() != null){
+            dfs(root.getLeftChild());
+        }
+        System.out.print(root.getValue()+" ");
+
+        if (root.getRightChild() != null){
+            dfs(root.getRightChild());
+        }
     }
 }
